cfd0487fa1854a2d8032a2b916d643bc4f8d670a
Chris Mills <cmills@breakfreesolutions.com>
2023-10-19 15:55:19 -0500

    updated readme, skeleton function added to parser

--- README.md
+++ README.md
@@ -2,13 +2,38 @@
 
 Demonstration code to illustrate possibilities during planned First Advantage workshop.
 
+# Installation
+
+To install, download the repo:
+
+    git clone https://github.com/break-free/first-advantage-demo
+    cd first-advantage-demo
+
+Setup and activate a Python virtual environment:
+
+    pip -m venv .venv-first-advantage-demo
+    source .venv-first-advantage-demo/bin/activate
+
+Run the demonstration:
+
+    python demo.py
+
+> [!NOTE]
+> Currently, the only option that works is conversation mode. The load matrix mode simply prints
+> to screen that the function needs to be completed.
+
 # TODO
+
+- [ ] Build a document store using the `parser.py` file.
+- [ ] Get the chat interface and document store linked up.
+
+# Completed TODO's
 
 - [x] Template files loaded to repo.
 
-Note that the files originate from this repo: 
+Note that the files originate from this repo:
 [breakfree-dk-with-openai-chat](https://github.com/break-free/breakfree-dk-with-openai-chat/tree/main)
 
-- [ ] Build a document store using the `parser.py` file.
-- [ ] Build a chat interface using the `chat.py` file.
-- [ ] Get the chat interface and document store linked up.
+- [x] Build a chat interface using the `chat.py` file.
+
+Basic functionality added so queries are sent and answers received from OpenAI.
--- parser.py
+++ parser.py
@@ -1,29 +1,32 @@
-# TODO: This whole file.
+# TODO: Update this function to parse a Microsoft Excel file using an available document
+#       loader.
+def parse():
+  print("Parse some data ... eventually. TODO!")
 
 
-trainingData = list(Path("training/facts/").glob("**/*.txt"))
-
-  #check there is data in the trainingData folder
-
-  if len(trainingData) < 1:
-    print("The folder training/facts should be populated with at least one .txt file.", file=sys.stderr)
-    return
-  
-  data = []
-  for training in trainingData:
-    with open(training) as f:
-      print(f"Add {f.name} to dataset")
-      data.append(f.read())
-  
-  textSplitter = CharacterTextSplitter(chunk_size=2000, separator="\n")
-  
-  docs = []
-  for sets in data:
-    docs.extend(textSplitter.split_text(sets))
-  
-  store = FAISS.from_texts(docs, OpenAIEmbeddings())
-  faiss.write_index(store.index, "training.index")
-  store.index = None
-  
-  with open("faiss.pkl", "wb") as f:
-    pickle.dump(store, f)
+# trainingData = list(Path("training/facts/").glob("**/*.txt"))
+#
+#   #check there is data in the trainingData folder
+#
+#   if len(trainingData) < 1:
+#     print("The folder training/facts should be populated with at least one .txt file.", file=sys.stderr)
+#     return
+#
+#   data = []
+#   for training in trainingData:
+#     with open(training) as f:
+#       print(f"Add {f.name} to dataset")
+#       data.append(f.read())
+#
+#   textSplitter = CharacterTextSplitter(chunk_size=2000, separator="\n")
+#
+#   docs = []
+#   for sets in data:
+#     docs.extend(textSplitter.split_text(sets))
+#
+#   store = FAISS.from_texts(docs, OpenAIEmbeddings())
+#   faiss.write_index(store.index, "training.index")
+#   store.index = None
+#
+#   with open("faiss.pkl", "wb") as f:
+#     pickle.dump(store, f)
